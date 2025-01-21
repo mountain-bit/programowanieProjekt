1aae15636abee34e9594978c6ef4e7508e17f849
GiLord7 <s61289@student.ubb.edu.pl>
2025-01-21 20:01:21 +0100

    Dodanie tury bota oraz aktualizacja main'a

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
 namespace ConsoleApp1
 {
-
     class Program
     {
         static void Main(string[] args)
         {
-            Plansza plansza = new Plansza(10);
-            plansza.wyswietl();
+            Console.WriteLine("Witaj w grze w statki!");
+            Console.Write("Podaj rozmiar planszy (np. 10): ");
+            int wielkoscPlanszy = int.Parse(Console.ReadLine());
 
+            Plansza graczPlansza = new Plansza(wielkoscPlanszy);
+            Plansza botPlansza = new Plansza(wielkoscPlanszy);
+
+            Console.WriteLine("\nTwoja plansza:");
+            graczPlansza.wyswietl();
+
+            while (true)
+            {
+                Console.WriteLine("\nTwoja tura:");
+                Console.Write("Podaj współrzędne strzału (x y): ");
+                string[] input = Console.ReadLine().Split(' ');
+                int x = int.Parse(input[0]);
+                int y = int.Parse(input[1]);
+
+                if (x < 0 || y < 0 || x >= wielkoscPlanszy || y >= wielkoscPlanszy)
+                {
+                    Console.WriteLine("Nieprawidłowe współrzędne. Spróbuj ponownie.");
+                    continue;
+                }
+
+                if (botPlansza.strzal(x, y))
+                {
+                    Console.WriteLine("Trafiłeś!");
+                }
+                else
+                {
+                    Console.WriteLine("Pudło!");
+                }
+
+                Console.WriteLine("Plansza przeciwnika:");
+                botPlansza.wyswietlDlaWrog();
+
+                if (StrzalBota.CzyGraZakonczona(botPlansza))
+                {
+                    Console.WriteLine("Gratulacje! Wygrałeś!");
+                    break;
+                }
+
+                Console.WriteLine("\nTura bota:");
+                StrzalBota.BotStrzal(graczPlansza);
+
+                Console.WriteLine("Twoja plansza:");
+                graczPlansza.wyswietl();
+
+                if (StrzalBota.CzyGraZakonczona(graczPlansza))
+                {
+                    Console.WriteLine("Przegrałeś! Bot zatopił wszystkie twoje statki.");
+                    break;
+                }
+            }
+
+            Console.WriteLine("Dziękujemy za grę!");
         }
     }
-
 }
-
---
+++ ConsoleApp1/ConsoleApp1/StrzalBota.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public static class StrzalBota
+    {
+        public static bool CzyGraZakonczona(Plansza plansza)
+        {
+            for (int i = 0; i < plansza.wielkosc; i++)
+            {
+                for (int j = 0; j < plansza.wielkosc; j++)
+                {
+                    if (plansza.plansza[i, j].stan == "zajęte")
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public static void BotStrzal(Plansza plansza)
+        {
+            Random rand = new Random();
+            int x, y;
+
+            do
+            {
+                x = rand.Next(0, plansza.wielkosc);
+                y = rand.Next(0, plansza.wielkosc);
+            }
+            while (plansza.plansza[x, y].stan == "trafione" || plansza.plansza[x, y].stan == "nietrafione");
+
+            if (plansza.strzal(x, y))
+            {
+                Console.WriteLine($"Bot trafił w ({x}, {y})!");
+            }
+            else
+            {
+                Console.WriteLine($"Bot chybił w ({x}, {y}).");
+            }
+        }
+    }
+}
